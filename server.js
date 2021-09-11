f9fd810a132b9b5003641d8d154f2e3701cb0945
Ryan Hillier <ryan1992186@gmail.com>
2021-09-11 16:32:22 -0500

    fixed login and logout and signup

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+  
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -34,5 +36,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("I'm listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
